fc6435f051aa72c5b95980f1cebd874cab02978f
Jorge Hortelano <jhortelano@biit-solutions.com>
2024-10-16 13:51:22 +0200

    Added submittedTime

--- src/main/java/com/biit/form/submitted/ISubmittedForm.java
+++ src/main/java/com/biit/form/submitted/ISubmittedForm.java
@@ -3,6 +3,8 @@ package com.biit.form.submitted;
 import com.biit.form.entity.IFormWithAnswers;
 import com.biit.form.submitted.implementation.SubmittedForm;
 
+import java.time.LocalDateTime;
+
 public interface ISubmittedForm extends ISubmittedObject, IFormWithAnswers {
 
     String getApplicationName();
@@ -16,5 +18,7 @@ public interface ISubmittedForm extends ISubmittedObject, IFormWithAnswers {
     SubmittedForm fromJson(String jsonString);
 
     String getSubmittedBy();
+
+    LocalDateTime getSubmittedAt();
 
 }
